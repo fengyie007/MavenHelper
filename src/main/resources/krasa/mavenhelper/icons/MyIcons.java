e0abe9ab1049194edff0d35fe03efe44918bf1cb
fengyie007 <fengyie007@gmail.com>
2021-09-10 15:24:48 +0800

    use gradle build

---
+++ src/main/resources/krasa/mavenhelper/icons/MyIcons.java
@@ -0,0 +1,13 @@
+package krasa.mavenhelper.icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+public class MyIcons {
+	public static final javax.swing.Icon ICON = IconLoader.getIcon("/krasa/mavenhelper/icons/debug.svg");
+	public static final javax.swing.Icon RUN_MAVEN_ICON = IconLoader.getIcon("/krasa/mavenhelper/icons/runMaven.svg");
+	public static final javax.swing.Icon PHASES_CLOSED = IconLoader.getIcon("/krasa/mavenhelper/icons/phasesClosed.svg");
+	public static final javax.swing.Icon PLUGIN_GOAL = IconLoader.getIcon("/krasa/mavenhelper/icons/pluginGoal.svg");
+	public static final javax.swing.Icon OPEN_TERMINAL = IconLoader.getIcon("/krasa/mavenhelper/icons/OpenTerminal.svg");
+	public static final javax.swing.Icon MAVEN_LOGO = IconLoader.getIcon("/krasa/mavenhelper/icons/mavenLogo.svg");
+
+}
